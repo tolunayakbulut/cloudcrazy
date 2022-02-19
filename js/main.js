c7c1129ab7169bb9d9132dd93d9aed1c805d0d43
tolunayakbulut <qame1375>
2022-02-19 22:08:50 +0300

    homepage completed highly. product sliders are created. footer is created.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 // Initialize Swiper
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".banners", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     spaceBetween: 30
+});
+var swiper = new Swiper(".featured", {
+    slidesPerView: 4,
+    spaceBetween: 16,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
 });
