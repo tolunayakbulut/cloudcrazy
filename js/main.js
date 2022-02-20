28271e13ab0426c9bfaffe5ed0751d9e9b1023e5
tolunayakbulut <qame1375>
2022-02-20 04:00:59 +0300

    PDP page is created. acordion menu is created.

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ var swiper = new Swiper(".featured", {
         prevEl: ".swiper-button-prev",
     }
 });
+
+
+// Accordion Menu
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + 18 + "px";
+            panel.style.height = panel.scrollHeight + 18 + "px";
+        }
+    });
+}
